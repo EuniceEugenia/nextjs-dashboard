5a61d14f72265cbe8971d4b3269aed6aaa994ef2
Eunice <euniceeugeniaa@gmail.com>
2026-03-24 11:12:10 +0700

    feat (Chapter 13-15): add authentication, metadata, and finish Dashboard App

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,7 +3,12 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import CardWrapper from '@/app/ui/dashboard/cards';
-// import { fetchCardData } from 'app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
+
 import { Suspense } from 'react';
 import { 
   RevenueChartSkeleton,
